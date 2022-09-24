879ba85fedbc41fe9a3b482ba00240dc444dc00b
Glrodrigo <gl.rodrigo18@gmail.com>
2022-09-23 23:35:58 -0300

    Project v.0.4

--- dia_semana/core/models.py
+++ dia_semana/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Dia_semanaModel(models.Model):
+    name        = models.CharField('Dia_semana', max_length = 50)
+    day         = models.IntegerField('Data')
+    month       = models.IntegerField('Mes')
+    modified_in = models.DateTimeField(
+        verbose_name = 'Modificado em',
+        auto_now_add = False, auto_now = True
+    )
+
+    def __str__(self):
+        return self.name
